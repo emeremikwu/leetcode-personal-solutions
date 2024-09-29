e0562b0671460310b787c6040f979bf3e29e846a
Emmanuel <princedee3@gmail.com>
2024-09-29 09:18:51 -0500

    22 generate parentheses

---
+++ 22.generate-parentheses.py
@@ -0,0 +1,47 @@
+from typing import List
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parentheses
+#
+
+# @lc code=start
+
+
+class Solution:
+  def generateParenthesis(self, n: int) -> List[str]:
+    stack = []
+    result = []
+
+    def backtrack(open_count: int, closed_count: int):
+      if open_count == closed_count == n:
+        result.append(''.join(stack))
+
+      if open_count < n:
+        stack.append('(')
+        backtrack(open_count + 1, closed_count)
+        stack.pop()
+
+      if closed_count < open_count:
+        stack.append(')')
+        backtrack(open_count, closed_count + 1)
+        stack.pop()
+
+    backtrack(0, 0)
+    return result
+
+# @lc code=end
+
+
+if __name__ == "__main__":
+  solution = Solution()
+  testlist = [
+      (1, ["()"]),
+      (2, ["(())", "()()"]),
+      (3, ["((()))", "(()())", "(())()", "()(())", "()()()"]),
+  ]
+  for n, expected in testlist:
+    result = solution.generateParenthesis(n)
+    print(result)
+    assert result == expected
+  print("All tests passed")
